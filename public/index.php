39627e366ac8b492075136224c318fc8c5b6f16a
ADMIN <huydo164@gmail.com>
2021-02-24 11:57:28 +0700

    update

--- public/index.php
+++ public/index.php
@@ -6,15 +6,15 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-header("Access-Control-Allow-Origin: *");   
-header("Content-Type: application/json; charset=UTF-8");    
-header("Access-Control-Allow-Methods: POST, DELETE, OPTIONS");    
-header("Access-Control-Max-Age: 3600");    
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");    
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {    
-    return 0;    
- } 
-
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, DELETE, OPTIONS");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return 0;
+ }
+ 
 define('LARAVEL_START', microtime(true));
 
 /*
